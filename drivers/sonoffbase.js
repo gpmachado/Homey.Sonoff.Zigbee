fb7f0504662e52b1f6e4c407cf86732832e559cf
Gabriel Pedrosa Machado <141525343+gpmachado@users.noreply.github.com>
2026-01-26 00:11:07 -0300

    Refactor SonoffBase to simplify attribute handling
    Removed debug logging and manufacturer ID handling from attribute read/write methods.

--- drivers/sonoffbase.js
+++ drivers/sonoffbase.js
@@ -4,10 +4,6 @@ const Homey = require('homey');
 const { ZigBeeDevice } = require('homey-zigbeedriver');
 const { debug, CLUSTER } = require('zigbee-clusters');
 
-if (process.env.DEBUG === "1") {
-    debug(true);
-}
-
 class SonoffBase extends ZigBeeDevice {
 
     _lastPollAttributes = null;
@@ -19,9 +15,6 @@ class SonoffBase extends ZigBeeDevice {
         options = options || {};
         this._lastPollAttributes = Date.now();
 
-        if (process.env.DEBUG === "1") {
-            this.enableDebug();
-        }
         this.printNode();
 
         if (options.noAttribCheck != true) {
@@ -33,10 +26,6 @@ class SonoffBase extends ZigBeeDevice {
         }
     }
 
-    /**
-     * Handle frame wrapper for battery polling
-     * Called on every Zigbee frame received
-     */
     _onHandleFrame(...args) {
         if (this._originalHandleFrame) {
             this._originalHandleFrame.apply(this, args);
@@ -76,37 +65,6 @@ class SonoffBase extends ZigBeeDevice {
         this.readAttribute(cluster, attr, handler);
     }
 
-    /**
-     * Execute a cluster operation with temporary manufacturer ID override
-     * @param {string} cluster - Cluster name
-     * @param {number|null} manufacturerId - Manufacturer ID to set (null to skip)
-     * @param {Function} operation - Async operation to execute with cluster
-     * @returns {Promise<any>} Operation result
-     */
-    async _withManufacturerId(cluster, manufacturerId, operation) {
-        const clusterObj = this.zclNode.endpoints[1].clusters[cluster];
-        if (!clusterObj) {
-            throw new Error(`Cluster ${cluster} not available on this device`);
-        }
-        
-        const originalManufacturerId = clusterObj.manufacturerId;
-        
-        try {
-            if (manufacturerId) {
-                clusterObj.manufacturerId = manufacturerId;
-            }
-            return await operation(clusterObj);
-        } finally {
-            if (manufacturerId) {
-                clusterObj.manufacturerId = originalManufacturerId;
-            }
-        }
-    }
-
-    /**
-     * Read attributes from a cluster
-     * Handles manufacturer-specific clusters (e.g., SonoffCluster 0xFC11)
-     */
     async readAttribute(cluster, attr, handler) {
         if ("NAME" in cluster) {
             cluster = cluster.NAME;
@@ -115,21 +73,10 @@ class SonoffBase extends ZigBeeDevice {
             attr = [attr];
         }
         
-        if (attr.length === 0) {
-            this.log(`No attributes to read`);
-            return;
-        }
-        
-        const manufacturerId = cluster === 'SonoffCluster' ? 0x1286 : null;
-        
         try {
             this.log(`Ask attribute`, attr);
             
-            const value = await this._withManufacturerId(
-                cluster,
-                manufacturerId,
-                (clusterObj) => clusterObj.readAttributes(...attr)
-            );
+            const value = await this.zclNode.endpoints[1].clusters[cluster].readAttributes(...attr);
             
             this.log(`Got attr`, attr, value);
             
@@ -149,10 +96,6 @@ class SonoffBase extends ZigBeeDevice {
         return this.writeAttributes(cluster, data);
     }
 
-    /**
-     * Write attributes to a cluster
-     * Handles manufacturer-specific clusters (e.g., SonoffCluster 0xFC11)
-     */
     async writeAttributes(cluster, attribs, filter = null) {
         if ("NAME" in cluster) {
             cluster = cluster.NAME;
@@ -180,14 +123,9 @@ class SonoffBase extends ZigBeeDevice {
 
         this.log(`Write attribute`, items);
         
-        const manufacturerId = cluster === 'SonoffCluster' ? 0x1286 : null;
-        
         try {
-            return await this._withManufacturerId(
-                cluster,
-                manufacturerId,
-                (clusterObj) => clusterObj.writeAttributes(items)
-            );
+            const result = await clusterObj.writeAttributes(items);
+            return result;
         } catch (error) {
             this.error(`Error write attr`, items, error);
             throw error;
