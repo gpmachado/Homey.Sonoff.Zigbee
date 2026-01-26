a9670eda9c531dc3f18c139c7fb2fb30d6f757ff
Gabriel Pedrosa Machado <141525343+gpmachado@users.noreply.github.com>
2026-01-25 22:22:23 -0300

    Refactor logging in SonoffBASICZBR3 class
    Updated logging messages for device initialization and deletion.

--- drivers/BASICZBR3/device.js
+++ drivers/BASICZBR3/device.js
@@ -3,26 +3,45 @@
 const { ZigBeeDevice } = require("homey-zigbeedriver");
 const { CLUSTER } = require('zigbee-clusters');
 
+/**
+ * Sonoff BASICZBR3 - Basic Zigbee Switch
+ * 
+ * Simple on/off switch without advanced Sonoff features.
+ * Does not support custom Sonoff clusters (0xFC11) or manufacturer-specific attributes.
+ */
 class SonoffBASICZBR3 extends ZigBeeDevice {
 
-  /**
-   * onNodeInit is called when the device is initialized.
-   */
-  async onNodeInit({ zclNode }) {
-    this.log('Device initialized');
-    this.printNode();
+    /**
+     * onNodeInit is called when the device is initialized.
+     */
+    async onNodeInit({ zclNode }) {
+        this.log(`BASICZBR3: Device initialized`);
+        this.printNode();
 
-    if (this.hasCapability('onoff')) {
-      this.registerCapability('onoff', CLUSTER.ON_OFF);
+        // Register on/off capability
+        if (this.hasCapability('onoff')) {
+            this.registerCapability('onoff', CLUSTER.ON_OFF);
+        }
+
+        // Configure attribute reporting for on/off state
+        this.configureAttributeReporting([
+            {
+                endpointId: 1,
+                cluster: CLUSTER.ON_OFF,
+                attributeName: 'onOff',
+                minInterval: 1,      // Minimum 1 second between reports
+                maxInterval: 3600,   // Maximum 1 hour (report at least once/hour)
+                minChange: 1         // Report on any state change
+            }
+        ]).catch(err => this.error(`Failed to configure attribute reporting:`, err));
     }
-  }
 
-  /**
-   * onDeleted is called when the user deleted the device.
-   */
-  async onDeleted() {
-    this.log("smartswitch removed");
-  }
+    /**
+     * onDeleted is called when the user deleted the device.
+     */
+    async onDeleted() {
+        this.log(`BASICZBR3: Switch removed`);
+    }
 
 }
 
